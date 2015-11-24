61aa7024cf53e6cf8d441339ace1d9943c95ead4
301git <jcadek@ualberta.ca>
2015-11-23 17:30:42 -0700

    stuff

--- ocean/landing.js
+++ ocean/landing.js
@@ -15,6 +15,6 @@ function expandPermissions(v){
 //a line expalining that roles acces
 function explainRole(r){
 	if(r == "a"){return "As an Administrator, you may create, view, remove and edit the information of all users and sensors"}
-	else if(r == "s"){return "As a Scientist, you may subscribe to sensors, search tyheir data and view an analysis of their data"}
+	else if(r == "s"){return "As a Scientist, you may subscribe to sensors, search their data and view an analysis of their data"}
 	else if(r == "d"){return "As a Data Curator, you may upload new data"}
 }
